428e80c9a79c02cfbf65f50d53ac32ab80dc4696
kizza <kieran.thorley@gmail.com>
2024-05-16 14:30:57 +0100

    Working get but id snack list and full put with id

--- server.js
+++ server.js
@@ -26,9 +26,58 @@ app.use(express.json())
 app.post('/snacks', async (req, res) => {
     console.log(req.body);
 
+    // const snack = await Snacks.create(req.body)
+
+    let doesSnackExist = await Snacks.exists({name: req.body.name});
+
+    
+console.log(doesSnackExist);
+
+
+    if(doesSnackExist) {
+      res.send('Already one of my snacks')
+} else {
     const snack = await Snacks.create(req.body)
     res.send(snack)
+
+    }
+
+
 })
+
+app.delete('/snacks', async (req, res) => {
+
+const snacks = await Snacks.deleteOne(req.body)
+
+res.send(snacks)
+})
+
+
+app.put('/snacks', async (req, res) => {
+    const snacks = await Snacks.updateOne(req.body, {rating: 2})
+
+    res.send(snacks)
+
+})
+
+app.get('/snacks/:snackId', async (req, res) =>{
+    console.log(req.params.snackId);
+const snack = await Snacks.findById(req.params.snackId)
+
+
+res.send(snack)
+})
+
+
+app.put('/snacks/:snackId', async (req, res) => {
+   console.log("hello");
+const snack = await Snacks.findById(req.params.snackId)
+
+let updatedSnack = await Snacks.updateOne(snack, req.body)
+
+res.send(updatedSnack)
+})
+
 
 app.listen(port, () => {
   console.log('Listening on port 3000');
